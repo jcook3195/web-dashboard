2776ab9d67ab348eb76328dee156663b79769d89
jcook3195 <jcook3195@gmail.com>
2023-07-31 09:33:54 -0400

    added saved recipes, fixed hard coded date modal

--- src/components/DateModal/DateModal.js
+++ src/components/DateModal/DateModal.js
@@ -3,9 +3,28 @@ import React from "react";
 import "./DateModal.scss";
 
 const DateModal = () => {
+  const months = {
+    0: "Jan",
+    1: "Feb",
+    2: "Mar",
+    3: "Apr",
+    4: "May",
+    5: "Jun",
+    6: "Jul",
+    7: "Aug",
+    8: "Sep",
+    9: "Oct",
+    10: "Nov",
+    11: "Dec",
+  };
+  const date = new Date();
+  const month = date.getMonth();
+
+  console.log(month);
+
   return (
     <div className="date-modal-container">
-      <h2>Jul 26</h2>
+      <h2>{months[6] + " " + date.getDate()}</h2>
     </div>
   );
 };
--- src/components/Widgets/Recipes/Recipes.js
+++ src/components/Widgets/Recipes/Recipes.js
@@ -4,7 +4,7 @@ import axios from "axios";
 import Button from "../../UIElements/Button";
 import Notice from "../../UIElements/Notice";
 
-// import { createRecipeDoc, getSavedRecipes } from "../../../firebase/crud";
+import { createRecipeDoc, getSavedRecipes } from "../../../firebase/crud";
 import "./Recipes.scss";
 
 const limitStrtingLength = (str) => {
@@ -18,28 +18,29 @@ const limitStrtingLength = (str) => {
   }
 };
 
-// const savedRecipes = await
+const formatMacroServings = (servings, macro) => {
+  const perServing = macro / servings;
+  const roundedMacroPerServing = Math.round(perServing);
 
-// savedRecipes.forEach((doc) => {
-//   console.log(doc.id, " => ", doc.data());
-// });
+  return roundedMacroPerServing;
+};
 
 const Recipes = () => {
   const [recipes, setRecipes] = useState([]);
-  // const [savedRecipes, setSavedRecipes] = useState([]);
-  // const [showNotice, setShowNotice] = useState(false);
-  // const [noticeMsg, setNoticeMsg] = useState("This is a notice");
-  // const [noticeType, setNoticeType] = useState("danger");
+  const [savedRecipes, setSavedRecipes] = useState([]);
+  const [showNotice, setShowNotice] = useState(false);
+  const [noticeMsg, setNoticeMsg] = useState("This is a notice");
+  const [noticeType, setNoticeType] = useState("danger");
 
-  // const noticeTimer = (msg) => {
-  //   setNoticeMsg(msg);
-  //   setShowNotice(true);
-  //   setNoticeType("success");
+  const noticeTimer = (msg) => {
+    setNoticeMsg(msg);
+    setShowNotice(true);
+    setNoticeType("success");
 
-  //   setTimeout(() => {
-  //     setShowNotice(false);
-  //   }, 5000);
-  // };
+    setTimeout(() => {
+      setShowNotice(false);
+    }, 5000);
+  };
 
   const newRecipesClickHandler = async () => {
     const result = await axios(
@@ -49,23 +50,46 @@ const Recipes = () => {
     setRecipes(result.data.hits);
   };
 
-  // const saveRecipeClickHandler = (data) => {
-  //   console.log("saving this recipe...");
-  //   const recipe = {
-  //     title: data.recipe.label,
-  //     url: data.recipe.url,
-  //     img: data.recipe.image,
-  //     servings: data.recipe.yield,
-  //     calories: data.recipe.calories / data.recipe.yield,
-  //     protein: data.recipe.totalNutrients.PROCNT.quantity / data.recipe.yield,
-  //     carbs: data.recipe.totalNutrients.CHOCDF.quantity / data.recipe.yield,
-  //     fats: data.recipe.totalNutrients.FAT.quantity / data.recipe.yield,
-  //   };
+  const saveRecipeClickHandler = (data) => {
+    console.log("saving this recipe...");
+    const imgUrl = data.recipe.image;
+    const recUrl = data.recipe.url;
+    const cleanImgUrl = imgUrl.replace(/['"]+/g, "");
+    const cleanRecUrl = recUrl.replace(/['"]+/g, "");
+    console.log("clean urls");
+    console.log(cleanImgUrl);
+    console.log(cleanRecUrl);
 
-  //   createRecipeDoc(recipe);
-  //   console.log(recipe);
-  //   noticeTimer(`${recipe.title} recipe has been saved.`);
-  // };
+    const recipe = {
+      title: data.recipe.label,
+      url: data.recipe.url,
+      img: data.recipe.image,
+      servings: data.recipe.yield,
+      calories: data.recipe.calories,
+      protein: data.recipe.totalNutrients.PROCNT.quantity,
+      carbs: data.recipe.totalNutrients.CHOCDF.quantity,
+      fats: data.recipe.totalNutrients.FAT.quantity,
+    };
+
+    createRecipeDoc(recipe);
+    console.log(recipe);
+    noticeTimer(`${recipe.title} recipe has been saved.`);
+    fetchSavedRecipesAfterSave();
+  };
+
+  const fetchSavedRecipesAfterSave = async () => {
+    let savedRecipes = [];
+    const result = await getSavedRecipes();
+
+    result.forEach((doc) => {
+      savedRecipes.push({
+        id: doc.id,
+        recipe: doc.data(),
+      });
+    });
+
+    setSavedRecipes(savedRecipes);
+  };
 
   useEffect(() => {
     const fetchRecipes = async () => {
@@ -76,31 +100,31 @@ const Recipes = () => {
       setRecipes(result.data.hits);
     };
 
-    // const fetchSavedRecipes = async () => {
-    //   let savedRecipes = [];
-    //   const result = await getSavedRecipes();
+    const fetchSavedRecipes = async () => {
+      let savedRecipes = [];
+      const result = await getSavedRecipes();
 
-    //   result.forEach((doc) => {
-    //     savedRecipes.push({
-    //       id: doc.id,
-    //       recipe: doc.data(),
-    //     });
-    //   });
+      result.forEach((doc) => {
+        savedRecipes.push({
+          id: doc.id,
+          recipe: doc.data(),
+        });
+      });
 
-    //   console.log("saved recipes new shit");
-    //   console.log(savedRecipes);
+      console.log("saved recipes new shit");
+      console.log(savedRecipes);
 
-    //   setSavedRecipes(result);
-    // };
+      setSavedRecipes(savedRecipes);
+    };
 
     fetchRecipes();
-    // fetchSavedRecipes();
+    fetchSavedRecipes();
   }, []);
 
-  // useEffect(() => {
-  //   console.log("SAVED RECIPES:");
-  //   console.log(savedRecipes);
-  // }, [savedRecipes]);
+  useEffect(() => {
+    console.log("SAVED RECIPES:");
+    console.log(savedRecipes);
+  }, [savedRecipes]);
 
   return (
     <React.Fragment>
@@ -135,24 +159,26 @@ const Recipes = () => {
                       <div className="col-12 text-center">
                         <p>
                           Cals-
-                          {Math.round(
-                            recipes[keyName].recipe.calories /
-                              recipes[keyName].recipe.yield
-                          )}{" "}
+                          {formatMacroServings(
+                            recipes[keyName].recipe.yield,
+                            recipes[keyName].recipe.calories
+                          )}
                           | P-
-                          {Math.round(
+                          {formatMacroServings(
+                            recipes[keyName].recipe.yield,
                             recipes[keyName].recipe.totalNutrients.PROCNT
-                              .quantity / recipes[keyName].recipe.yield
+                              .quantity
                           )}
                           g | C-
-                          {Math.round(
+                          {formatMacroServings(
+                            recipes[keyName].recipe.yield,
                             recipes[keyName].recipe.totalNutrients.CHOCDF
-                              .quantity / recipes[keyName].recipe.yield
+                              .quantity
                           )}
                           g | F-
-                          {Math.round(
-                            recipes[keyName].recipe.totalNutrients.FAT
-                              .quantity / recipes[keyName].recipe.yield
+                          {formatMacroServings(
+                            recipes[keyName].recipe.yield,
+                            recipes[keyName].recipe.totalNutrients.FAT.quantity
                           )}
                           g
                         </p>
@@ -160,14 +186,93 @@ const Recipes = () => {
                     </div>
                   </div>
                 </a>
+                <div className="card-subfooter pt-2 pb-2 w-100">
+                  <div className="row">
+                    <div className="col-12 text-center">
+                      <Button
+                        classNames="btn-custom"
+                        onClickEvent={() =>
+                          saveRecipeClickHandler(recipes[keyName])
+                        }
+                      >
+                        Save Recipe
+                      </Button>
+                    </div>
+                  </div>
+                </div>
               </div>
             ))}
           </div>
         </div>
         <div className="col-12 text-center">
           <Button classNames="btn-custom" onClickEvent={newRecipesClickHandler}>
-            New Recipes
+            Load New Recipes
           </Button>
+        </div>
+      </div>
+      {showNotice && <Notice alertType={noticeType}>{noticeMsg}</Notice>}
+      <div className="row saved-recipes-row mt-5 mb-5">
+        <div className="col-12 text-center">
+          <hr className="mb-5" />
+          <h1>Saved Recipes</h1>
+        </div>
+        <div className="col-12">
+          {Object.keys(savedRecipes).map((key) => {
+            const recImgUrl = savedRecipes[key].recipe.recipe.img;
+            const recUrl = savedRecipes[key].recipe.recipe.url;
+            const cleanImgUrl = recImgUrl.replace(/['"]+/g, "");
+            const cleanRecUrl = recUrl.replace(/['"]+/g, "");
+
+            return (
+              <div
+                className="row justify-content-center"
+                key={savedRecipes[key]}
+              >
+                <div className="col-8">
+                  <a href={cleanRecUrl} target="_blank" rel="noreferrer">
+                    <div className="saved-recipe-container d-flex justify-content-between align-items-center mt-2 mb-2 p-2 pt-4 pb-4">
+                      <div className="saved-recipe-img-container">
+                        <img
+                          src={cleanImgUrl}
+                          alt={savedRecipes[key].recipe.recipe.title + " image"}
+                        />
+                      </div>
+                      <div className="saved-recipe-title-container text-center">
+                        <h4>{savedRecipes[key].recipe.recipe.title}</h4>
+                      </div>
+                      <div className="saved-recipe-info-container text-center">
+                        <p>
+                          Serves:
+                          {" " + savedRecipes[key].recipe.recipe.servings}{" "}
+                          <br />
+                          Cals:
+                          {formatMacroServings(
+                            savedRecipes[key].recipe.recipe.servings,
+                            savedRecipes[key].recipe.recipe.calories
+                          ) + " "}
+                          | P:
+                          {formatMacroServings(
+                            savedRecipes[key].recipe.recipe.servings,
+                            savedRecipes[key].recipe.recipe.protein
+                          ) + " "}
+                          | C:
+                          {formatMacroServings(
+                            savedRecipes[key].recipe.recipe.servings,
+                            savedRecipes[key].recipe.recipe.carbs
+                          ) + " "}
+                          | F:
+                          {formatMacroServings(
+                            savedRecipes[key].recipe.recipe.servings,
+                            savedRecipes[key].recipe.recipe.fats
+                          )}
+                        </p>
+                      </div>
+                    </div>
+                  </a>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
     </React.Fragment>
